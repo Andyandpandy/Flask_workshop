d818a24996177256f6e6720417627523b7612a52
andyandpandy <andreas.beckerbertelsen@hotmail.com>
2018-04-03 22:20:35 +0200

    Step six Use the session object to create a session for a user thereby knowing whether the user has been logged in or not. This requires a secret_key for the application.

--- FlaskWorkshop/FlaskWorkshop.py
+++ FlaskWorkshop/FlaskWorkshop.py
@@ -1,14 +1,20 @@
-from flask import Flask, request, render_template, redirect, url_for, abort
+from flask import Flask, request, render_template, redirect, url_for, abort, session
+import os
 
 from connection import Firebase_conn
 
 app = Flask(__name__)
 
+app.secret_key = os.urandom(24)
+
 conn = Firebase_conn()
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    if session.get('username'):
+        return 'Hello World!'
+    return redirect(url_for('login'))
 
 
 @app.route('/simple_login/', methods=['GET', 'POST'])
@@ -30,19 +36,25 @@ def login():
         return render_template('login.html')
 
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        app.logger.error(username)
-        app.logger.error(password)
+        try:
+            username = request.form['username']
+            password = request.form['password']
+            app.logger.error(username)
+            app.logger.error(password)
 
-        #Attempt to find user on firebase
-        attempted_username = conn.get_user(username)
+            # Attempt to find user on firebase
+            attempted_username = conn.get_user(username)
 
-        if attempted_username and attempted_username['password'] == password:
-            app.logger.error("User is {} and the password matched: {}".format(attempted_username, attempted_username['password'] == password))
-            return redirect(url_for('home_page'))
+            if attempted_username and attempted_username['password'] == password:
+                app.logger.error("User is {} and the password matched: {}".format(attempted_username,
+                                                                                  attempted_username[
+                                                                                      'password'] == password))
+                session['username'] = username
+                return redirect(url_for('home_page'))
 
-        app.logger.error("The users password is {}".format(conn.get_user('Andy')))
+            app.logger.error("The users password is {}".format(conn.get_user('Andy')))
+        except KeyError:
+            pass
         return redirect(url_for('login'))
     else:
         abort(403)
@@ -50,17 +62,22 @@ def login():
 
 @app.route('/home/')
 def home_page():
-    return render_template('home.html')
+    if session.get('username'):
+        return render_template('home.html')
+    return redirect(url_for('login'))
 
 
 @app.route('/settings/')
 def settings_page():
-    return render_template('settings.html')
+    if session.get('username'):
+        return render_template('settings.html')
+    return redirect(url_for('login'))
 
 
 @app.route('/logout/')
 def logout():
-    return redirect(url_for('simple_login'))
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
