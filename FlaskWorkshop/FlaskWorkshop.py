c413dfeba8376781b9ed80c25b2ee53b47120ae7
andyandpandy <andreas.beckerbertelsen@hotmail.com>
2018-04-03 22:02:45 +0200

    Step five Use the request.form to get data from the html form and into the flask application. Here we check to see if the username and password match the ones on the firebase.

--- FlaskWorkshop/FlaskWorkshop.py
+++ FlaskWorkshop/FlaskWorkshop.py
@@ -6,18 +6,6 @@ app = Flask(__name__)
 
 conn = Firebase_conn()
 
-"""
-Step four involves using the firebase module (created by Ozgur Vatansever).
-The Firebase application has a class known as FirebaseApplication, which needs the base url for the firebase.
-The firebase module also has the possibility to add authentication for firebase to secure that its not everyone who 
-can access the database.
-The connection.py file contains two classes one with the connections for firebase and another with a simple exception.
-The create_a_user() method is used to create a user and the get_user method is used to get the same user from the 
-database.
-Also when the user is found this is logged (line 33).
-"""
-
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -32,6 +20,30 @@ def simple_login():
         # Find user Andy
         app.logger.error("The users password is {}".format(conn.get_user('Andy')))
         return redirect(url_for('home_page'))
+    else:
+        abort(403)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        app.logger.error(username)
+        app.logger.error(password)
+
+        #Attempt to find user on firebase
+        attempted_username = conn.get_user(username)
+
+        if attempted_username and attempted_username['password'] == password:
+            app.logger.error("User is {} and the password matched: {}".format(attempted_username, attempted_username['password'] == password))
+            return redirect(url_for('home_page'))
+
+        app.logger.error("The users password is {}".format(conn.get_user('Andy')))
+        return redirect(url_for('login'))
     else:
         abort(403)
 
