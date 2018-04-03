b2665c3e2a10209b98f3c5b0899e46b91e1af2fe
andyandpandy <andreas.beckerbertelsen@hotmail.com>
2018-04-03 21:36:18 +0200

    Step four Use the firebase module to either post, put, patch, delete or get from the firebase database. We create a simple class where we use this.

--- FlaskWorkshop/FlaskWorkshop.py
+++ FlaskWorkshop/FlaskWorkshop.py
@@ -1,6 +1,21 @@
 from flask import Flask, request, render_template, redirect, url_for, abort
 
+from connection import Firebase_conn
+
 app = Flask(__name__)
+
+conn = Firebase_conn()
+
+"""
+Step four involves using the firebase module (created by Ozgur Vatansever).
+The Firebase application has a class known as FirebaseApplication, which needs the base url for the firebase.
+The firebase module also has the possibility to add authentication for firebase to secure that its not everyone who 
+can access the database.
+The connection.py file contains two classes one with the connections for firebase and another with a simple exception.
+The create_a_user() method is used to create a user and the get_user method is used to get the same user from the 
+database.
+Also when the user is found this is logged (line 33).
+"""
 
 
 @app.route('/')
@@ -14,6 +29,8 @@ def simple_login():
         return render_template('login.html')
 
     if request.method == 'POST':
+        # Find user Andy
+        app.logger.error("The users password is {}".format(conn.get_user('Andy')))
         return redirect(url_for('home_page'))
     else:
         abort(403)
