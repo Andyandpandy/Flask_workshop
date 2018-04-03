e9ebd8b3c6649e1d96852a81c464628152e75b35
andyandpandy <andreas.beckerbertelsen@hotmail.com>
2018-04-03 20:13:39 +0200

    Step two Use Get and Post methods, redirect(), url_for(), render_template() & request.

--- FlaskWorkshop/FlaskWorkshop.py
+++ FlaskWorkshop/FlaskWorkshop.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/simple_login/', methods=['GET', 'POST'])
+def simple_login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        print("post")
+        return redirect(url_for('hello_world'))
 
 
 if __name__ == '__main__':
